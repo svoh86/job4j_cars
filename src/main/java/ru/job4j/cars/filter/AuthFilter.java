db161e5acb38d1b448f63f62f11de0d95bf04d4c
svoh86 <sms-86@mail.ru>
2023-03-14 15:49:45 +0300

    Добавлен фильтр для неавторизованных пользователей

---
+++ src/main/java/ru/job4j/cars/filter/AuthFilter.java
@@ -0,0 +1,46 @@
+package ru.job4j.cars.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Set;
+
+/**
+ * Класс фильтр.
+ * Если запрос идет к адресам index или user/loginPage и т.д., то мы их пропускаем сразу.
+ * Если запросы идут к другим адресам, то проверяем наличие пользователя в HttpSession.
+ * Если его нет, то мы переходим на страницу авторизации.
+ *
+ * @author Svistunov Mikhail
+ * @version 1.0
+ */
+@Component
+public class AuthFilter implements Filter {
+    private final Set<String> uriSet = Set.of(
+            "index", "posts", "user/loginPage", "user/login", "user/add", "user/create");
+
+    private boolean matchUri(String uri) {
+        return uriSet.stream().anyMatch(uri::endsWith);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+        String uri = req.getRequestURI();
+        if (matchUri(uri)) {
+            filterChain.doFilter(req, res);
+            return;
+        }
+        if (req.getSession().getAttribute("user") == null) {
+            res.sendRedirect(req.getContextPath() + "/user/loginPage");
+            return;
+        }
+        filterChain.doFilter(req, res);
+    }
+}
